c8cf3d0d65ad941a2583b74e0af2d0a9d876bcca
Chris Martine <cmmartine@gmail.com>
2023-03-03 07:26:19 -0500

    Added functions for displaying books and added some styling

--- scriptLibrary.js
+++ scriptLibrary.js
@@ -1,10 +1,10 @@
-let myLibrary = [ ];
+let myLibrary = [ {title: 'Harry and the Not So Secret Chamber', author: 'JK', pages: '560', readStatus: 'Not Read'}, {title: 'The Storm Before the Storm: The Beginning of the End of the Roman Republic', author: 'Mike Duncan', pages: '352', readStatus: 'Read'}, {title: 'A Common-Sense Guide to Data Structures and Algorithms, Second Edition: Level Up Your Core Programming Skills', author: 'Jay Wengrow', pages: '507', readStatus: 'In Progress'} ];
 
 function Book(title, author, pages) {
   this.title = title
   this.author = author
   this.pages = pages
-  this.readStatus = false
+  this.readStatus = 'Not Read'
 }
 
 const submitBtn = document.getElementById('submit');
@@ -18,5 +18,27 @@ function addBookToLibrary(e) {
   let pagesInput = document.getElementById('pages').value;
   let newBook = new Book( titleInput, authorInput, pagesInput );
   myLibrary.push(newBook);
-  document.getElementById('bookForm').reset();
+  document.getElementById('book-form').reset();
+  insertBookHTML(newBook);
 }
+
+const bookContainer = document.getElementById('book-container');
+
+function insertBookHTML(book) {
+  let bookCard = document.createElement('p');
+  bookCard.classList.add('book-card');
+  let title = document.createTextNode(`Title: ${book.title}\n\n`);
+  let author = document.createTextNode(`Author: ${book.author}\n\n`);
+  let pages = document.createTextNode(`Pages: ${book.pages}\n\n`) ;
+  let status = document.createTextNode(`Status: ${book.readStatus}`);
+  bookCard.append(title, author, pages, status);
+  bookContainer.appendChild(bookCard);
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    insertBookHTML(book);
+  });
+}
+
+displayBooks();
