a219b042dca2775d09ea2d0cd935640b338834b5
Chris Martine <cmmartine@gmail.com>
2023-03-06 09:28:14 -0500

    Added change status button, edited removeBook function

--- scriptLibrary.js
+++ scriptLibrary.js
@@ -13,6 +13,7 @@ const bookShowBtn = document.getElementById('show');
 const bookHideBtn = document.getElementById('hide');
 const bookForm = document.getElementById('book-form');
 let removeBookBtns; /* Query Selector filled upon displaying initial books and each time a new book is inserted */
+let changeStatusBtns;
 bookShowBtn.addEventListener('click', showNewBookForm);
 bookHideBtn.addEventListener('click', hideNewBookForm);
 submitBtn.addEventListener('click', addBookToLibrary);
@@ -35,15 +36,22 @@ function insertBookHTML(book, index) {
   let title = document.createTextNode(`Title: ${book.title}\n\n`);
   let author = document.createTextNode(`Author: ${book.author}\n\n`);
   let pages = document.createTextNode(`Pages: ${book.pages}\n\n`) ;
-  let status = document.createTextNode(`Status: ${book.readStatus} \n\n`);
+  let status = document.createElement('p');
+  status.classList.add('book-status');
+  status.append(`Status: ${book.readStatus} \n\n`);
   const button = document.createElement('button');
   button.innerText = 'Remove Book';
   button.className = 'remove-book';
-  button.dataset.index = index; 
-  bookCard.append(title, author, pages, status, button);
+  button.dataset.index = index;
+  const statusBtn = document.createElement('button');
+  statusBtn.innerText = 'Change Status';
+  statusBtn.className = 'change-status';
+  statusBtn.dataset.index = index;
+  bookCard.append(title, author, pages, status, statusBtn, button);
   bookContainer.appendChild(bookCard);
   removeBookBtns = document.querySelectorAll('.remove-book');
-  assignRemoveBookButtons()
+  changeStatusBtns = document.querySelectorAll('.change-status');
+  assignBookButtons()
 }
 
 function displayBooks() {
@@ -53,7 +61,8 @@ function displayBooks() {
     i += 1;
   });
   removeBookBtns = document.querySelectorAll('.remove-book');
-  assignRemoveBookButtons()
+  changeStatusBtns = document.querySelectorAll('.change-status');
+  assignBookButtons()
 }
 
 function showNewBookForm(e) {
@@ -68,19 +77,40 @@ function hideNewBookForm(e) {
   bookHideBtn.classList.add('toggle-visibility');
 }
 
-function assignRemoveBookButtons() {
+function assignBookButtons() {
   removeBookBtns.forEach((currentBtn) => {
     currentBtn.addEventListener('click', removeBook);
+  });
+  changeStatusBtns.forEach((currentBtn) => {
+    currentBtn.addEventListener('click', changeStatus);
   });
  }
 
 function removeBook(e) {
   let bookCards = document.querySelectorAll('.book-card');
-  myLibrary.splice(e.target.dataset.index, 1);
+  myLibrary[e.target.dataset.index] = null;
   bookCards.forEach((book) => {
     if (book.dataset.index === e.target.dataset.index) {
       book.remove();
     };
+  });
+}
+
+function changeStatus(e) {
+  let book = myLibrary[e.target.dataset.index]
+  let bookCards = document.querySelectorAll('.book-card');
+  if (book.readStatus === 'Not Read') {
+    book.readStatus = 'In Progress';
+  } else if (book.readStatus === 'In Progress') {
+    book.readStatus = 'Read';
+  } else {
+    book.readStatus = 'Not Read';
+  }
+  bookCards.forEach((bookCard) => {
+    if (bookCard.dataset.index === e.target.dataset.index) {
+      let statusText = bookCard.querySelector('.book-status');
+      statusText.innerText = `Status: ${book.readStatus}`;
+    }
   });
 }
 
